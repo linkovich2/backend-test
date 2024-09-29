0f2c959ee5bb2d35b4cd9e880fc87443890e1410
Jonathon <kaos78414@yahoo.com>
2024-09-28 22:03:34 -0500

    Test filtered responses

--- server/test/index.spec.ts
+++ server/test/index.spec.ts
@@ -5,6 +5,8 @@ import request from 'supertest'
 import gql from 'graphql-tag'
 import { print } from 'graphql/language/printer.js'
 
+import util from 'util'
+
 const server = create_server(3001)
 
 describe('GraphQL API', () => {
@@ -202,13 +204,127 @@ describe('GraphQL API', () => {
     describe("with filter applied", () => {
       describe("on locations", () => {
         it("should combine only the related records", async () => {
+          const db_task = await context.prisma.tasks.findFirst({
+            where: {
+              complete: true
+            },
+            include: {
+              location: {
+                include: {
+                  tasks: {
+                    include: {
+                      logged_times: {
+                        include: { worker: true }
+                      }
+                    }
+                  }
+                }
+              },
+              logged_times: true
+            }
+          })
 
+
+          const query = gql`
+            query Locations($ids: [Int!], $complete: Boolean, $worker_ids: [Int!]) {
+              locations(ids: $ids) { 
+                id
+                name 
+                total_cost(complete: $complete, worker_ids: $worker_ids)
+              }
+            }
+          `
+          const worker_ids = [ db_task?.logged_times[0].worker_id ]
+          const response = await request(server)
+            .post('/')
+            .send({ 
+              query: print(query),
+              variables: {
+                ids: [ db_task?.location_id ],
+                worker_ids: worker_ids,
+                complete: true
+              }
+            })
+            .set('Accept', 'application/json')
+          
+          const body = response.body
+          // console.log(util.inspect(body, { showHidden: false, depth: null, colors: true }))
+          expect(body).to.have.property('data')
+          expect(body.data).to.have.property('locations').that.is.an('array')
+          expect(body.data.locations.length).to.eq(1)
+      
+          const location = body.data.locations[0]
+          expect(location.id).to.eq(db_task?.location.id.toString())
+          let total_cost = 0.0
+
+          db_task?.location.tasks.forEach((task) => {
+            if (task.complete) {
+              task.logged_times.forEach((lt) => {
+                if (worker_ids.includes(lt.worker_id)) {
+                  total_cost += (lt.time_seconds / 3600) * lt.worker.hourly_wage.toNumber()
+                }
+              })
+            }
+          })
+
+          expect(Math.ceil(total_cost)).to.eq(Math.ceil(location.total_cost))
         })
       })
       
       describe("on workers", () => {
         it("should combine only the related records", async () => {
+          const db_worker = await context.prisma.workers.findFirst({
+            include: {
+              logged_times: {
+                include: {
+                  task: {
+                    include: {
+                      location: true
+                    }
+                  }
+                }
+              }
+            }
+          })
 
+          const query = gql`
+            query Workers($location_ids: [Int!], $ids: [Int!], $complete: Boolean) {
+              workers(ids: $ids) { 
+                id
+                hourly_wage 
+                total_cost(location_ids: $location_ids, complete: $complete)
+              }
+            }
+          `
+          const location_ids = [ db_worker?.logged_times[0].task.location.id ]
+          const response = await request(server)
+            .post('/')
+            .send({ 
+              query: print(query),
+              variables: {
+                ids: [ db_worker?.id ],
+                location_ids: location_ids,
+                complete: true
+              }
+            })
+            .set('Accept', 'application/json')
+          
+          const body = response.body
+          expect(body).to.have.property('data')
+          expect(body.data).to.have.property('workers').that.is.an('array')
+          expect(body.data.workers.length).to.eq(1)
+      
+          const worker = body.data.workers[0]
+          let total_cost = 0.0
+
+          db_worker?.logged_times.forEach((lt) => {
+            if (lt.task.complete && location_ids.includes(lt.task.location.id)) {
+              total_cost += (lt.time_seconds / 3600) * db_worker?.hourly_wage.toNumber()
+            }
+          })
+
+          // JS is doing some weird stuff with the Decimal type here on occasion, so ceil is here as a @temp fix
+          expect(Math.ceil(total_cost)).to.eq(Math.ceil(worker.total_cost))
         })
       })
     })
